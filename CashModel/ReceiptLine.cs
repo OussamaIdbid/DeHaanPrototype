a3230d69f83048644ac5882c17ba10aa1628fb59
Kerolos <k.ghobreyal@icloud.com>
2019-11-27 15:08:22 +0100

    Commit

---
+++ CashModel/ReceiptLine.cs
@@ -0,0 +1,16 @@
+﻿namespace CashModel
+{
+    public class ReceiptLine
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Quantity * Price;
+            }
+        }
+    }
+}
